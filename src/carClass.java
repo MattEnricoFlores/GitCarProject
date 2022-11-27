c6a9f200457148df3c8afd3b124a970fcd8d76f3
MattEnricoFlores <mattenricoflores2022@gmail.com>
2022-11-27 12:45:28 +0200

    Initial commit

---
+++ src/carClass.java
@@ -0,0 +1,31 @@
+
+public class carClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//Car Class
+class car
+{
+	String brand;
+	String model;
+	int amountOfFuel = 10;
+	
+	
+	public void brand()
+	{
+		brand ="";
+	}
+	public void model()
+	{
+		model = "";
+	}
+	public void amountOfFuel()
+	{
+		amountOfFuel=10;
+	}
+	
+}
